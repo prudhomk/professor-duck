0ef09661a0a5eb96c40ff4845201c5363451a9d5
Kalan Prudhomme <cleaver9@gmail.com>
2021-11-22 14:18:55 -0800

    added additional phrases

--- src/components/data/Phrases.js
+++ src/components/data/Phrases.js
@@ -7,6 +7,12 @@ export const phrases = [
   { phrase: 'Have you tried `npm i`?' },
   { phrase: 'Don\'t be a hero, copy+paste links.' },
   { phrase: 'What are you trying to do?' }, 
+  { phrase: 'Go for a walk, clearing your head can help.' },
+  { phrase: 'I agree, Regex does suck!' },
+  { phrase: 'Why didn\'t the duck cross the road? He didn\'t want to step on a quack!' },
+  { phrase: 'It\'s ok, let it all out.' },
+  { phrase: 'Bug fixing isn\'t all it\'s quacked up to be' },
+  { phrase: 'I\'ll try and wing it!' } 
 ];
 
 export const sfx = [
